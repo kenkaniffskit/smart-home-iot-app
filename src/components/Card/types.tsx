573762266ca3364e959cdc9557fcaf9a5341c200
kenkaniffskit <keynnn25@gmail.com>
2023-09-11 14:12:09 +0800

    changed the ui of buttons

--- src/components/Card/types.tsx
+++ src/components/Card/types.tsx
@@ -1,5 +1,5 @@
 export type DevicesTypes = {
-	id: number;
-	name: string;
-	value: string;
+  id: number;
+  name: string;
+  value: string;
 };
--- src/constants/devices.tsx
+++ src/constants/devices.tsx
@@ -1,12 +1,28 @@
 export const devices = [
-	{ id: 1, name: "LIGHT 1", value: "SWITCH_01_IS_OPEN" },
-	{ id: 2, name: "LIGHT 2", value: "SWITCH_02_IS_OPEN" },
-	{ id: 3, name: "LIGHT 3", value: "SWITCH_03_IS_OPEN" },
-	{ id: 4, name: "FAN 1", value: "SWITCH_04_IS_OPEN" },
-	// { name: "LED 05", value: "LED_05_IS_OPEN" },
-	// { name: "LED 06", value: "LED_06_IS_OPEN" },
-	// { name: "LED 07", value: "LED_07_IS_OPEN" },
-	// { name: "LED 08", value: "LED_08_IS_OPEN" },
-	// { name: "LED 09", value: "LED_09_IS_OPEN" },
-	// { name: "LED 10", value: "LED_10_IS_OPEN" },
+  {
+    id: 1,
+    name: "LIGHT 1",
+    value: "SWITCH_01_IS_OPEN",
+  },
+  {
+    id: 2,
+    name: "LIGHT 2",
+    value: "SWITCH_02_IS_OPEN",
+  },
+  {
+    id: 3,
+    name: "LIGHT 3",
+    value: "SWITCH_03_IS_OPEN",
+  },
+  {
+    id: 4,
+    name: "FAN 1",
+    value: "SWITCH_04_IS_OPEN",
+  },
+  // { name: "LED 05", value: "LED_05_IS_OPEN" },
+  // { name: "LED 06", value: "LED_06_IS_OPEN" },
+  // { name: "LED 07", value: "LED_07_IS_OPEN" },
+  // { name: "LED 08", value: "LED_08_IS_OPEN" },
+  // { name: "LED 09", value: "LED_09_IS_OPEN" },
+  // { name: "LED 10", value: "LED_10_IS_OPEN" },
 ];
